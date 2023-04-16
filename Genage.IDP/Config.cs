78c6cac49061df5c6d52064bf90d57b200fa84d8
George <nuriszka@gmail.com>
2023-04-16 13:19:43 +0200

    configuring idp

--- Genage.IDP/Config.cs
+++ Genage.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Genage.IDP;
 
@@ -17,5 +18,30 @@ public static class Config
 
     public static IEnumerable<Client> Clients =>
         new Client[]
-            { };
+            {
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = true,
+                    RedirectUris =
+                    {
+                        "https://localhost:7184/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://localhost:7184/signout-callback-oidc"
+                    }
+                }
+            };
 }
